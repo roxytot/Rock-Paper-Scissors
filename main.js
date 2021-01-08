310a4b713280d5cc799975ed63bbf269cfb0873e
Thibaut <thibaut_marechal@yahoo.com>
2021-01-08 12:18:20 +0000

    Task 5 - confirm ()  step 1

--- main.js
+++ main.js
@@ -101,6 +101,50 @@
 
 
 // TASK 4
+// function getWinner(player1, player2) {
+//     if (player1 === "rock") {
+//         if (player2 === "rock") {
+//             return 0;
+//         } else if (player2 === "paper") {
+//             return -1;
+//         } else {
+//             return 1;
+//         }
+//     } else if (player1 === "paper") {
+//         if (player2 === "rock") {
+//             return 1;
+//         } else if (player2 === "paper") {
+//             return 0;
+//         } else {
+//             return -1;
+//         }
+//     } else if (player1 === "scissors") {
+//         if (player2 === "rock") {
+//             return -1;
+//         } else if (player2 === "paper") {
+//             return 1;
+//         } else {
+//             return 0;
+//         }
+//     }
+// }
+
+// let moveComputer = ["scissors", "paper", "rock"]
+
+// function randomMoveComp() {
+//     let randNumber = Math.floor(Math.random() * 2); //selecting random integer number between 0 and 2
+//     console.log(`Computer chose ${randNumber} from our array`); //printing random number to console
+//     console.log(moveComputer[randNumber]); // print random answer (string)
+//     return moveComputer[randNumber]; // return random answer
+// }
+
+// let answerPlayer1 = prompt("Enter your move");
+// let answerRandComp = randomMoveComp();
+// let result = getWinner(answerPlayer1, answerRandComp);
+// alert(`The result is ${result}`);
+
+
+// TASK 5
 function getWinner(player1, player2) {
     if (player1 === "rock") {
         if (player2 === "rock") {
@@ -142,3 +186,7 @@ let answerPlayer1 = prompt("Enter your move");
 let answerRandComp = randomMoveComp();
 let result = getWinner(answerPlayer1, answerRandComp);
 alert(`The result is ${result}`);
+
+let confirmChoice = confirm("Do you want to keep playing play?")
+
+while (confirm)
