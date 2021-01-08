dfe8f1f0b65915b28074a4f65c420165d29a2081
Roxana Tot <roxana.tot@gmail.com>
2021-01-08 10:58:08 +0000

    Rock case written

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("this is a test")
+console.log("this is a test");
+
+let playerMove = "rock";
+let computerMove = "paper";
+
+if (playerMove === "rock") {
+    if (computerMove === "rock") {
+        console.log('draw');
+    } else if (computerMove === "paper") {
+        console.log("lose");
+    } else {
+        console. log("win");
+    }
+} else if (playerMove === "paper")
