6e2bab466603013256783ab9609db1f7293acab8
Thibaut <thibaut_marechal@yahoo.com>
2021-01-08 11:29:24 +0000

    Task 2 test and complete

--- main.js
+++ main.js
@@ -30,43 +30,36 @@
 // }
 
 // TASK 2 - Function
-function getWinner(player1, player2) {  //player2 computer
+
+//results: 0 is draw, 1 is win, -1 is lose
+// player 1 is playerMove, player2 is computerMove
+function getWinner(player1, player2) {
     if (player1 === "rock") {
         if (player2 === "rock") {
-            console.log('draw');
             return 0;
         } else if (player2 === "paper") {
             return -1;
-            console.log("lose");
         } else {
             return 1;
-            console.log("win");
         }
     } else if (player1 === "paper") {
         if (player2 === "rock") {
             return 1;
-            console.log('win');
         } else if (player2 === "paper") {
             return 0;
-            console.log("draw");
         } else {
             return -1;
-            console.log("lose");
         }
     } else if (player1 === "scissors") {
         if (player2 === "rock") {
             return -1;
-            console.log('lose');
         } else if (player2 === "paper") {
             return 1;
-            console.log("win");
         } else {
             return 0;
-            console.log("draw");
         }
     }
 }
 
 let result = getWinner("rock", "rock");
-getWinner();
 console.log(`The result is ${result}`);
